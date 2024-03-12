9635a8eca1eacb7bd0552b535967711aec340905
User h <bektur.duyshenbekuulu@iaau.edu.kg>
2024-03-12 17:06:02 +0600

    exception lms

--- src/Main.java
+++ src/Main.java
@@ -19,25 +19,25 @@ public class Main {
                             System.out.print("Write length: ");
                             int length = scanner.nextInt();
                             if (length<0){
-                                throw new MyException("Терс сан болбойт ");
+                                throw new MyException();
 
                             }
                             System.out.print("Write width: ");
                             int width = scanner.nextInt();
                             if (width<0){
-                                throw new MyException("Терс сан болбойт ");
+                                throw new MyException();
                             }
                             System.out.print("Write height: ");
                             int height = scanner.nextInt();
                             if (height<0) {
-                                throw new MyException("Терс сан болбойт ");
+                                throw new MyException();
 
                             }
                             Parallelepiped parallelepiped = new Parallelepiped(length, width, height);
                             System.out.println("\nParallelepiped Area: " + parallelepiped.ParallelepipedArea());
                             System.out.println("Parallelepiped Volume:" + parallelepiped.ParallelepipedVolume() + "\n");
                         } catch (MyException e) {
-                            System.out.println(e.getMessage());
+                            System.out.println("Терс сан болбойт ");
 
                         }
                     }
@@ -47,13 +47,13 @@ public class Main {
                             System.out.print("Write height: ");
                             int height = scanner.nextInt();
                             if (height<0){
-                                throw new MyException("Терс сан болбойт ");
+                                throw new MyException();
                             }
 
                             System.out.print("Write radius: ");
                             int radius = scanner.nextInt();
                             if (radius < 0) {
-                                throw new MyException("Терс сан болбойт ");
+                                throw new MyException();
                             }
 
                             Cylinder cylinder = new Cylinder(height,radius);
@@ -61,7 +61,7 @@ public class Main {
                             System.out.println("Cylinder Volume: " + cylinder.CylinderVolume() + "\n");
 
                         } catch (MyException e) {
-                            System.out.println(e.getMessage());
+                            System.out.println("Терс сан болбойт ");
                         }
                   }
               }
